74bf46a8570a6ab36595d7c88039fbf7ed04606f
Alex Vzorov <alex.vzorov@gmail.com>
2015-12-12 04:33:40 +0300

    Use state transformer and state monad, finally

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import System.IO
-import qualified System.IO.Strict as S (hGetContents)
+import System.IO.Strict as S
 import System.Environment (getArgs)
 
 import Control.Monad (liftM)
@@ -18,8 +18,10 @@ import Data.List (sort)
 import qualified Data.Map.Strict as M
 
 import Text.HTML.TagSoup
-import Web.Scotty
+import qualified Web.Scotty as WS
 import Network.Wai.Middleware.RequestLogger
+
+import Control.Monad.State.Strict
 
 -- isDataTable == table with a header
 table_ :: String
@@ -99,55 +101,51 @@ attackFromColumns r = Attack
   <*> readInt (r!!4)
   <*> Just (r!!5)
 
-getContentsOf :: FilePath -> IO String
-getContentsOf filepath =
-    withFile filepath ReadMode $ \h -> do
-        hSetEncoding h latin1
-        putStrLn $ "Reading " ++ filepath
-        S.hGetContents h
-
 type Dict = M.Map String [Attack]
 
-dictionaryWithKey :: (Attack -> String) -> [Attack] -> Dict
-dictionaryWithKey keyFn xs = M.fromListWith (++) [(keyFn x, [x]) | x <- xs] where
+mkDict :: (Attack -> String) -> [Attack] -> Dict
+mkDict keyFn xs = M.fromListWith (++) [(keyFn x, [x]) | x <- xs] where
 
-paginate   :: [a] -> ActionM [a]
+paginate   :: [a] -> WS.ActionM [a]
 paginate xs = do
-    limit <- param "limit" `rescue` (return . const 25)
-    page  <- param "page"  `rescue` (return . const 0)
+    limit <- WS.param "limit" `WS.rescue` (return . const 25)
+    page  <- WS.param "page"  `WS.rescue` (return . const 0)
     return $ paginate' page limit xs where
         paginate' page limit = take limit . drop (page * limit)
 
-filterByCity   :: [Attack] ->ActionM [Attack]
-filterByCity xs = do city' <- param "city"
-                     return . filter (\x ->city' == city x) $ xs
+filterByCity :: [Attack] ->WS.ActionM [Attack]
+filterByCity xs = liftM filterCities $ WS.param "city" where
+  filterCities c = filterOn city (c==) xs
+  filterOn f p = filter (p . f)
 
--- lookupParam  :: String -> Dict -> ActionM [Attack]
-lookupParam n dict = do x <- param n
-                        return $ list x dict
+lookupParam x dict = liftM (`find` dict) $ WS.param x where
+  find k = fromMaybe [] . M.lookup k
 
-lookupCountry :: Dict -> ActionM [Attack]
+lookupCountry :: Dict -> WS.ActionM [Attack]
 lookupCountry  = lookupParam "country"
 
-lookupCity :: Dict -> ActionM [Attack]
+lookupCity :: Dict -> WS.ActionM [Attack]
 lookupCity  = lookupParam "city"
 
-list :: String -> Dict -> [Attack]
-list k = fromMaybe [] . M.lookup k
+readAttacks :: FilePath -> StateT [Attack] IO ()
+readAttacks fname = do
+  a <- liftIO $ withFile fname ReadMode $ \h -> do
+    hSetEncoding h latin1
+    putStrLn $ "Reading " ++ fname
+    S.hGetContents h
 
-attacks :: [FilePath] -> IO [Attack]
-attacks files = do
-    contents <- concat <$> mapM getContentsOf files
-    return . sort . map (fromJust . attackFromColumns) . filter (not . null) . extractData $ contents
+  let attacks = map (fromJust . attackFromColumns) . filter (not . null) . extractData $ a
+  modify' (mappend attacks)
 
 loadAll :: [FilePath] -> IO (Dict,Dict)
 loadAll files = do
-    atks <- attacks files
+    unsorted <- execStateT (mapM_ readAttacks files) []
+    let atks = sort unsorted
 
     putStrLn "Building dictionaries..."
 
-    let countriesDict = dictionaryWithKey country atks
-    let citiesDict    = dictionaryWithKey city atks
+    let countriesDict = mkDict country atks
+    let citiesDict    = mkDict city atks
 
     -- force evaluation of dictionaries
     putStrLn $ "Total attacks: " ++ (show . length) atks
@@ -158,26 +156,24 @@ loadAll files = do
     return (countriesDict,citiesDict)
 
 
-startServer :: Int -> Dict -> Dict -> IO ()
-startServer port citiesDict countriesDict = scotty port $ do
-    middleware logStdoutDev
+startServer :: Int -> (Dict, Dict) -> IO ()
+startServer port (countriesDict,citiesDict) = WS.scotty port $ do
+    WS.middleware logStdoutDev
 
-    get "/cities" . json . M.keys $ citiesDict
-    get "/countries" . json . M.keys $ countriesDict
+    WS.get "/cities" . WS.json . M.keys $ citiesDict
+    WS.get "/countries" . WS.json . M.keys $ countriesDict
 
-    get "/cities/:city" $
-        lookupCity citiesDict >>= paginate >>= json
+    WS.get "/cities/:city" $
+        lookupCity citiesDict >>= paginate >>= WS.json
 
-    get "/countries/:country" $
-        lookupCountry countriesDict >>= paginate >>= json
+    WS.get "/countries/:country" $
+        lookupCountry countriesDict >>= paginate >>= WS.json
 
-    get "/countries/:country/:city" $
-        lookupCountry countriesDict >>= filterByCity >>= paginate >>= json
+    WS.get "/countries/:country/:city" $
+        lookupCountry countriesDict >>= filterByCity >>= paginate >>= WS.json
 
-    get "/headers" $ liftM (map (\(k,v) -> k `LT.append` LT.pack ": " `LT.append` v)) headers >>= json
+    WS.get "/headers" $ liftM (map (\(k,v) -> k `LT.append` LT.pack ": " `LT.append` v)) WS.headers >>= WS.json
 
 
 main :: IO ()
-main = do
-    (countriesDict,citiesDict) <- getArgs >>= loadAll
-    startServer 3000 citiesDict countriesDict
+main = getArgs >>= loadAll >>= startServer 3000
