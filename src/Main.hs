76396768da6b3aa3b27695acbfb05159096a505a
Alex Vzorov <alex.vzorov@gmail.com>
2017-09-10 10:37:27 +0900

    Reformat source

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
+import System.Environment (getArgs)
 import System.IO
 import System.IO.Strict as S
-import System.Environment (getArgs)
-
-import Control.Monad (liftM)
 import Control.Applicative
-
+import Control.Monad (liftM)
 import Data.Aeson.Types
 import Data.Hourglass
+import Data.List (sort)
+import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Text (pack)
 import qualified Data.Text.Lazy as LT
-import Data.List (sort)
-import qualified Data.Map.Strict as M
-
-import qualified Web.Scotty as WS
 import Network.Wai.Middleware.RequestLogger
-
+import qualified Web.Scotty as WS
 import Control.Monad.State.Strict
-
 import Scraper
 
 instance Ord Attack where
     compare a b = compare (date a) (date b)
 
 instance ToJSON Attack where
-    toJSON (Attack date country city killed injured description) = object
-      ["date" .= date
-      ,"country" .= country
-      ,"city" .= city
-      ,"killed" .= killed
-      ,"injured" .= injured
-      ,"description" .= description
-      ]
+    toJSON Attack{..} =
+        object
+            [ "date" .= date
+            , "country" .= country
+            , "city" .= city
+            , "killed" .= killed
+            , "injured" .= injured
+            , "description" .= description]
 
 instance ToJSON DateTime where
     toJSON time = String $ pack $ timePrint ISO8601_Date time
@@ -43,76 +39,78 @@ instance ToJSON DateTime where
 type Dict = M.Map String [Attack]
 
 mkDict :: (Attack -> String) -> [Attack] -> Dict
-mkDict keyFn xs = M.fromListWith (++) [(keyFn x, [x]) | x <- xs] where
+mkDict keyFn xs = M.fromListWith (++) [(keyFn x, [x]) | x <- xs]
 
-paginate   :: [a] -> WS.ActionM [a]
+paginate :: [a] -> WS.ActionM [a]
 paginate xs = do
     limit <- WS.param "limit" `WS.rescue` (return . const 25)
-    page  <- WS.param "page"  `WS.rescue` (return . const 0)
-    return $ paginate' page limit xs where
-        paginate' page limit = take limit . drop (page * limit)
+    page <- WS.param "page" `WS.rescue` (return . const 0)
+    return $ paginate' page limit xs
+  where
+    paginate' page limit = take limit . drop (page * limit)
 
-filterByCity :: [Attack] ->WS.ActionM [Attack]
-filterByCity xs = liftM filterCities $ WS.param "city" where
-  filterCities c = filterOn city (c==) xs
-  filterOn f p = filter (p . f)
+filterByCity :: [Attack] -> WS.ActionM [Attack]
+filterByCity xs = filterCities <$> WS.param "city"
+  where
+    filterCities c = filterOn city (c ==) xs
+    filterOn f p = filter (p . f)
 
-lookupParam x dict = liftM (`find` dict) $ WS.param x where
-  find k = fromMaybe [] . M.lookup k
+lookupParam x dict = (`find` dict) <$> WS.param x
+  where
+    find k = fromMaybe [] . M.lookup k
 
 lookupCountry :: Dict -> WS.ActionM [Attack]
-lookupCountry  = lookupParam "country"
+lookupCountry = lookupParam "country"
 
 lookupCity :: Dict -> WS.ActionM [Attack]
-lookupCity  = lookupParam "city"
+lookupCity = lookupParam "city"
 
 readAttacks :: FilePath -> StateT [Attack] IO ()
 readAttacks fname = do
-  a <- liftIO $ withFile fname ReadMode $ \h -> do
-    hSetEncoding h latin1
-    putStrLn $ "Reading " ++ fname
-    S.hGetContents h
+    a <-
+        liftIO $
+        withFile fname ReadMode $ \h -> do
+            hSetEncoding h latin1
+            putStrLn $ "Reading " ++ fname
+            S.hGetContents h
+    let attacks =
+            map (fromJust . attackFromColumns) . filter (not . null) . extractData $ a
+    modify' (mappend attacks)
 
-  let attacks = map (fromJust . attackFromColumns) . filter (not . null) . extractData $ a
-  modify' (mappend attacks)
-
-loadAll :: [FilePath] -> IO (Dict,Dict)
+loadAll :: [FilePath] -> IO (Dict, Dict)
 loadAll files = do
     unsorted <- execStateT (mapM_ readAttacks files) []
     let atks = sort unsorted
-
     putStrLn "Building dictionaries..."
-
     let countriesDict = mkDict country atks
-    let citiesDict    = mkDict city atks
-
+    let citiesDict = mkDict city atks
     -- force evaluation of dictionaries
     putStrLn $ "Total attacks: " ++ (show . length) atks
     putStrLn $ "In countries: " ++ (show . length) (M.keys countriesDict)
     putStrLn $ "In cities: " ++ (show . length) (M.keys citiesDict)
     putStrLn $ "Total killed: " ++ (show . sum . map killed) atks
     putStrLn $ "Total injured: " ++ (show . sum . map injured) atks
-    return (countriesDict,citiesDict)
-
+    return (countriesDict, citiesDict)
 
 startServer :: Int -> (Dict, Dict) -> IO ()
-startServer port (countriesDict,citiesDict) = WS.scotty port $ do
-    WS.middleware logStdoutDev
-
-    WS.get "/cities" . WS.json . M.keys $ citiesDict
-    WS.get "/countries" . WS.json . M.keys $ countriesDict
-
-    WS.get "/cities/:city" $
-        lookupCity citiesDict >>= paginate >>= WS.json
-
-    WS.get "/countries/:country" $
-        lookupCountry countriesDict >>= paginate >>= WS.json
-
-    WS.get "/countries/:country/:city" $
-        lookupCountry countriesDict >>= filterByCity >>= paginate >>= WS.json
-
-    WS.get "/headers" $ liftM (map (\(k,v) -> k `LT.append` LT.pack ": " `LT.append` v)) WS.headers >>= WS.json
-
+startServer port (countriesDict,citiesDict) =
+    WS.scotty port $ do
+        WS.middleware logStdoutDev
+        WS.get "/cities" . WS.json . M.keys $ citiesDict
+        WS.get "/countries" . WS.json . M.keys $ countriesDict
+        WS.get "/cities/:city" $ lookupCity citiesDict >>= paginate >>= WS.json
+        WS.get "/countries/:country" $
+            lookupCountry countriesDict >>= paginate >>= WS.json
+        WS.get "/countries/:country/:city" $
+            lookupCountry countriesDict >>= filterByCity >>= paginate >>= WS.json
+        WS.get "/headers" $
+            fmap
+                (map
+                    (\(k,v) ->
+                          k `LT.append` LT.pack ": " `LT.append` v))
+                WS.headers >>=
+            WS.json
 
 main :: IO ()
+-- main = main1
 main = getArgs >>= loadAll >>= startServer 3000
--- src/Scraper.hs
+++ src/Scraper.hs
@@ -3,38 +3,44 @@ module Scraper where
 import Text.HTML.TagSoup
 import Data.Hourglass
 import Data.Maybe
+import System.Environment (getArgs)
+import qualified Data.Conduit as C
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
+import Data.Conduit.Filesystem
+import Control.Monad.Trans.Resource
+import Network.HTTP.Conduit
+import Control.Monad.Trans (liftIO)
+import Control.Monad
+
+table_ = "<table>" :: String
+
+th_ = "<th>" :: String
+
+tr_ = "<tr>" :: String
+
+td_ = "<td>" :: String
 
 -- isDataTable == table with a header
-table_ :: String
-table_ = "<table>"
-
-th_ :: String
-th_    = "<th>"
-
-tr_ :: String
-tr_    = "<tr>"
-
-td_ :: String
-td_    = "<td>"
-
 isDataTable :: [Tag String] -> Bool
-isDataTable = not.null.partitions (~== th_).head.partitions (~== tr_)
+isDataTable = not . null . partitions (~== th_) . head . partitions (~== tr_)
 
-data Attack = Attack { date :: DateTime
-                     , country :: String
-                     , city :: String
-                     , killed :: Integer
-                     , injured :: Integer
-                     , description :: String
-                     }
-                     deriving (Eq,Show)
+data Attack = Attack
+    { date :: !DateTime
+    , country :: !String
+    , city :: !String
+    , killed :: !Integer
+    , injured :: !Integer
+    , description :: !String
+    } deriving (Eq,Show)
 
 -- takes a html text and returns a list of attacks
 extractData :: String -> [[String]]
-extractData = map (normaliseColumns . extractColumns) . extractRows . extractTable . extractTags
+extractData =
+    map (normaliseColumns . extractColumns) . extractRows . extractTable . extractTags
 
 extractTags :: String -> [Tag String]
-extractTags = canonicalizeTags.parseTags
+extractTags = canonicalizeTags . parseTags
 
 extractTable :: [Tag String] -> [Tag String]
 extractTable = concatMap tail . filter isDataTable . partitions (~== table_)
@@ -43,23 +49,44 @@ extractRows :: [Tag String] -> [[Tag String]]
 extractRows = partitions (~== tr_)
 
 extractColumns :: [Tag String] -> [[String]]
-extractColumns = map (map (unwords.words.fromTagText).filter isTagText).partitions (~== td_)
+extractColumns =
+    map (map (unwords . words . fromTagText) . filter isTagText) . partitions (~== td_)
 
 normaliseColumns :: [[String]] -> [String]
 normaliseColumns = map $ fromMaybe "" . listToMaybe
 
 readInt :: String -> Maybe Integer
 readInt "" = Just 0
-readInt s  = Just $ read s
+readInt s = Just $ read s
 
 readDate :: String -> Maybe DateTime
-readDate  = timeParse [Format_Year4,Format_Text '.',Format_Month2, Format_Text '.', Format_Day2]
+readDate = timeParse
+    [Format_Year4, Format_Text '.', Format_Month2, Format_Text '.', Format_Day2]
 
 attackFromColumns :: [String] -> Maybe Attack
 attackFromColumns r = Attack
-  <$> readDate (head r)
-  <*> Just (r!!1)
-  <*> Just (r!!2)
-  <*> readInt (r!!3)
-  <*> readInt (r!!4)
-  <*> Just (r!!5)
+    <$> readDate (head r)
+    <*> Just (r !! 1)
+    <*> Just (r !! 2)
+    <*> readInt (r !! 3)
+    <*> readInt (r !! 4)
+    <*> Just (r !! 5)
+
+sourceList :: Monad m => [o] -> C.Source m o
+sourceList = mapM_ C.yield
+
+sink :: C.Sink String IO ()
+sink = C.awaitForever (liftIO . putStrLn)
+
+main1 = do
+    files <- getArgs
+    runResourceT $ sourceDirectory "." C.$$ C.awaitForever (liftIO . putStrLn)
+    -- runResourceT $ CB.sourceFile (head files) C.$$ C.awaitForever (liftIO . print)
+    -- request <- parseUrl "http://thereligionofpeace.com/"
+    -- manager <- newManager tlsManagerSettings
+    -- runResourceT $ do
+    --   response <- http request manager
+    --   responseBody response C.$$+- CB.sinkFile "current.htm"
+    sourceList
+        [1, 2, 3] C.$$
+        CL.mapM_ print
